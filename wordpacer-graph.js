4ca4249c97205188988de2e1dcab0afcf38bf1cd
Jason van Gumster <jason@monsterjavaguns.com>
2016-05-02 21:21:19 -0400

    Code re-organization, interface tweak, performance bump
      * Point labels were severly hurting graph draw time, so had to nix 'em
      * Moved graph drawing and Markdown rendering to their own functions
          (now they render right from the start and the Markdown text
          updates while typing)
      * Disabling point labels also means that the Show Labels checkbox no
          longer makes sense to include. So commented out for now.
      * Added debug prints to time data gathering and graph drawing
      * Included rotated axis labels for Word Length and Reader Velocity
      * Removed x-axis ticks
      * Limited y-axis velocity values to 20 units above and below max and
          min velocities.
      * I think that there's still a problem with velocity calculation. It
          never goes above 250 or below 100. Not sure if that's right.

--- wordpacer-graph.js
+++ wordpacer-graph.js
@@ -1,117 +1,160 @@
+function markitdown(source) {
+    var html = source;
+    $('#result-html').html(html);
+    var text = strip(html); // Get plain text XXX assumes Markdown -> HTML
+    $('#debug').html(text); //DEBUG
+    return text;
+}
+function graphText(text) {
+    console.log('Gathering data...'); //DEBUG
+    var start = performance.now(); //DEBUG
+    wordData = gatherData(text);
+    var end = performance.now(); //DEBUG
+    console.log('Gathered. (', end - start, 'ms)'); //DEBUG
+
+    var stats = {
+        avgWordlength: 0,
+        minVelocity: Math.min.apply(null, wordData.velocities),
+        maxVelocity: Math.max.apply(null, wordData.velocities),
+        avgVelocity: (wordData.velocities.reduce(function(sum, x) {return sum + x;}, 0)) / wordData.velocities.length
+    };
+    var allLetters = 0;
+    for (var i = 0; i < wordData.wordlengths.length; i++) {
+        for (var j = 0; j < wordData.wordlengths[i].length; j++) {
+            allLetters += wordData.wordlengths[i][j];
+        }
+    }
+    stats.avgWordlength = allLetters / wordData.wordcount;
+    $('#result-stats #wordcount').html(wordData.wordcount);
+    $('#result-stats #sentcount').html(wordData.sentcount);
+    $('#result-stats #paracount').html(wordData.paracount);
+    $('#result-stats #avg_wordlength').html(stats.avgWordlength);
+    $('#result-stats #min_velocity').html(stats.minVelocity);
+    $('#result-stats #max_velocity').html(stats.maxVelocity);
+    $('#result-stats #avg_velocity').html(stats.avgVelocity);
+    //console.log(wordData.wordlengths, stats.avgWordlength); //DEBUG
+
+    start = performance.now(); //DEBUG
+    console.log('Assembling graph data...'); //DEBUG
+    var ticks = Array.apply(null, Array(wordData.wordcount + 2)).map(function(_, i) {return i;}); // the +2 is needed
+    var wordCombo = [];
+    var velocityCombo = [];
+    velocityCombo.push([0,0]);
+    for (var i = 0; i < wordData.wordticks.length; i++) {
+        wordCombo[i] = [];
+        for (var j = 0; j < wordData.wordticks[i].length; j++) {
+            wordCombo[i][j] = [wordData.wordticks[i][j], wordData.wordlengths[i][j], wordData.allWords[i][j]];
+            velocityCombo.push([wordData.wordticks[i][j], wordData.velocities[wordData.wordticks[i][j]-1]]);
+            //console.log('i:',i,'j:',j,'wordcombo:',wordCombo[i][j]); //DEBUG
+        }
+    }
+    //console.log(velocityCombo); //DEBUG
+    wordCombo.push(velocityCombo);
+    end = performance.now(); //DEBUG
+    console.log('Assembled. (', end - start, 'ms)'); //DEBUG
+    start = performance.now(); //DEBUG
+    console.log('Creating graph...');
+    var plotSeries = [];
+    for (var i = 0; i < wordCombo.length - 1; i++) {
+        plotSeries.push({});
+    }
+    plotSeries.push({
+        renderer: $.jqplot.LineRenderer,
+        color: '#f00',
+        yaxis: 'y2axis',
+        showMarker: false
+    });
+    var paragraphRectangles = makeParagraphRectangles(wordData.paragraphStarts);
+    //console.log(wordCombo) //DEBUG
+    $.jqplot.config.enablePlugins = true;
+    //console.log($('#wordchart').width() / wordData.wordcount); //DEBUG
+    var plot1 = $.jqplot('wordchart', wordCombo, {
+        // Only animate if we're not using excanvas (not in IE 7 or IE 8)..
+        animate: !$.jqplot.use_excanvas,
+        animateReplot: true,
+        seriesColors: ['#889', '#ccd'],
+        seriesDefaults: {
+            renderer: $.jqplot.BarRenderer,
+            rendererOptions: {
+                barWidth: $('#wordchart').width() / wordData.wordcount,
+                barPadding: - $('#wordchart').width() / wordData.wordcount,
+                barMargin: 0
+            },
+            pointLabels: { show: false },
+        },
+        series: plotSeries,
+        axes: {
+            xaxis: {
+                //renderer: $.jqplot.CategoryAxisRenderer,
+                ticks: ticks,
+                showTicks: false
+            },
+            yaxis: {
+                rendererOptions: {
+                    forceTickAt0: true
+                },
+                label: 'Word Length',
+                labelRenderer: $.jqplot.CanvasAxisLabelRenderer
+            },
+            y2axis: {
+                rendererOptions: {
+                    alignTicks: true,
+                    forceTickAt0: true
+                },
+                min: stats.minVelocity - 20,
+                max: stats.maxVelocity + 20,
+                label: 'Reader Velocity',
+                labelRenderer: $.jqplot.CanvasAxisLabelRenderer
+            }
+        },
+        canvasOverlay: {
+            show: true,
+            objects: paragraphRectangles,
+        },
+        highlighter: { show: false }
+    });
+    //plot1.series[plot1.series.length - 1].renderer = $.jqplot.LineRenderer();
+    //console.log(plot1.series[plot1.series.length - 1]); //DEBUG
+    end = performance.now(); //DEBUG
+    console.log('Graphed. (', end - start, 'ms)'); //DEBUG
+
+    $('#wordchart').bind('jqplotDataClick',
+        function (ev, seriesIndex, pointIndex, data) {
+            seriesIndex++;
+            pointIndex++;
+            if (data.length == 3) {
+                $('#info').html('sentence: '+seriesIndex+', word: '+pointIndex+' ('+data[2]+'), word length: '+data[1]);
+            }
+            else {
+                $('#info').html('sentence: '+seriesIndex+', word: '+pointIndex+', reader velocity: '+data[1]+' WPM');
+            }
+        }
+    );
+    plot1.replot({
+        resetAxes: true,
+        axes: {
+            y2axis: {
+                min: stats.minVelocity - 20,
+                max: stats.maxVelocity + 20
+            }
+        }
+    });
+}
+
 $(document).ready(function () {
     var md = window.markdownit({typographer: true});
+    var text;
+
+    text = markitdown(md.render($('#source').val()));
+    graphText(text);
+
+    $('#source').keyup(function () {
+        text = markitdown(md.render($('#source').val()));
+    });
 
     $('#doit').click(function () {
-        var html = md.render($('#source').val());
-        $('#result-html').html(html);
-
-        var text = strip(html); // Get plain text XXX assumes Markdown -> HTML
-        wordData = gatherData(text);
-
-        var stats = {
-            avgWordlength: 0,
-            minVelocity: Math.min.apply(null, wordData.velocities),
-            maxVelocity: Math.max.apply(null, wordData.velocities),
-            avgVelocity: (wordData.velocities.reduce(function(sum, x) {return sum + x;}, 0)) / wordData.velocities.length
-        };
-        var allLetters = 0;
-        for (var i = 0; i < wordData.wordlengths.length; i++) {
-            for (var j = 0; j < wordData.wordlengths[i].length; j++) {
-                allLetters += wordData.wordlengths[i][j];
-            }
-        }
-        stats.avgWordlength = allLetters / wordData.wordcount;
-        $('#result-stats #wordcount').html(wordData.wordcount);
-        $('#result-stats #sentcount').html(wordData.sentcount);
-        $('#result-stats #paracount').html(wordData.paracount);
-        $('#result-stats #avg_wordlength').html(stats.avgWordlength);
-        $('#result-stats #min_velocity').html(stats.minVelocity);
-        $('#result-stats #max_velocity').html(stats.maxVelocity);
-        $('#result-stats #avg_velocity').html(stats.avgVelocity);
-        //console.log(wordData.wordlengths, stats.avgWordlength); //DEBUG
-
-        var ticks = Array.apply(null, Array(wordData.wordcount + 2)).map(function(_, i) {return i;}); // the +2 is needed
-        var wordCombo = [];
-        var velocityCombo = [];
-        velocityCombo.push([0,0]);
-        for (var i = 0; i < wordData.wordticks.length; i++) {
-            wordCombo[i] = [];
-            for (var j = 0; j < wordData.wordticks[i].length; j++) {
-                wordCombo[i][j] = [wordData.wordticks[i][j], wordData.wordlengths[i][j], wordData.allWords[i][j]];
-                velocityCombo.push([wordData.wordticks[i][j], wordData.velocities[wordData.wordticks[i][j]-1]]);
-                //console.log('i:',i,'j:',j,'wordcombo:',wordCombo[i][j]); //DEBUG
-            }
-        }
-        //console.log(velocityCombo); //DEBUG
-        wordCombo.push(velocityCombo);
-        var plotSeries = [];
-        for (var i = 0; i < wordCombo.length - 1; i++) {
-            plotSeries.push({});
-        }
-        plotSeries.push({
-            renderer: $.jqplot.LineRenderer,
-            color: '#f00',
-            yaxis: 'y2axis'
-        });
-        var paragraphRectangles = makeParagraphRectangles(wordData.paragraphStarts);
-        //console.log(wordCombo) //DEBUG
-        $.jqplot.config.enablePlugins = true;
-        //console.log($('#wordchart').width() / wordData.wordcount); //DEBUG
-        var plot1 = $.jqplot('wordchart', wordCombo, {
-            // Only animate if we're not using excanvas (not in IE 7 or IE 8)..
-            animate: !$.jqplot.use_excanvas,
-            animateReplot: true,
-            seriesColors: ['#889', '#ccd'],
-            seriesDefaults: {
-                renderer: $.jqplot.BarRenderer,
-                rendererOptions: {
-                    barWidth: $('#wordchart').width() / wordData.wordcount,
-                    barPadding: - $('#wordchart').width() / wordData.wordcount,
-                    barMargin: 0
-                },
-                pointLabels: { show: true },
-            },
-            series: plotSeries,
-            axes: {
-                xaxis: {
-                    //renderer: $.jqplot.CategoryAxisRenderer,
-                    ticks: ticks
-                },
-                yaxis: {
-                    rendererOptions: {
-                        forceTickAt0: true
-                    }
-                },
-                y2axis: {
-                    rendererOptions: {
-                        alignTicks: true,
-                        forceTickAt0: true
-                    }
-                }
-            },
-            canvasOverlay: {
-                show: true,
-                objects: paragraphRectangles,
-            },
-            highlighter: { show: false }
-        });
-        //plot1.series[plot1.series.length - 1].renderer = $.jqplot.LineRenderer();
-        //console.log(plot1.series[plot1.series.length - 1]); //DEBUG
-
-        $('#wordchart').bind('jqplotDataClick',
-            function (ev, seriesIndex, pointIndex, data) {
-                seriesIndex++;
-                pointIndex++;
-                if (data.length == 3) {
-                    $('#info').html('sentence: '+seriesIndex+', word: '+pointIndex+' ('+data[2]+'), word length: '+data[1]);
-                }
-                else {
-                    $('#info').html('sentence: '+seriesIndex+', word: '+pointIndex+', reader velocity: '+data[1]+' WPM');
-                }
-            }
-        );
-        plot1.replot({ resetAxes: true });
-        $('#show_labels').click(
+        graphText(text);
+/*        $('#show_labels').click(
             function() {
                 //var $checkbox = $(this);
                 console.log($(this).prop('checked'));
@@ -122,7 +165,6 @@ $(document).ready(function () {
                     $('.jqplot-point-label').css('display', 'none');
                 }
             }
-        );
-        $('#debug').html(text); //DEBUG
+        );*/
     });
 });
