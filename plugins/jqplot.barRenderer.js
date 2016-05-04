87389045e1b53e52f7d88b80adca352f010d37d1
Jason van Gumster <jason@monsterjavaguns.com>
2016-05-03 22:00:46 -0400

    Small addition to try and help with properly resizing bars on zoom.
    Doesn't seem to work, though.

--- plugins/jqplot.barRenderer.js
+++ plugins/jqplot.barRenderer.js
@@ -593,7 +593,7 @@
         var pointx, points, pointy, nvals, nseries, pos;
         
         if (this._stack && this.shadow) {
-            if (this.barWidth == null) {
+            if (this.barWidth == null || this.rendererOptions.barWidth == null) {
                 this.renderer.setBarWidth.call(this);
             }
         
